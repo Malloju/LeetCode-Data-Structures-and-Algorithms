145b4b73861fa8401e941435555709156ad2ed02
Malloju Vishwam <152184740+Malloju@users.noreply.github.com>
2026-02-25 14:46:24 +0530

    Update 215_Kth_Largest_Element_in_an_Array.cpp

--- Heap./215_Kth_Largest_Element_in_an_Array.cpp
+++ Heap./215_Kth_Largest_Element_in_an_Array.cpp
@@ -1,14 +1,30 @@
+
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int>q;
+//         int n=nums.size();
+//         for(int i=0;i<n;i++){
+//             q.push(nums[i]);
+//         }
+//         for(int i=0;i<k-1;i++){
+//             q.pop();
+//         }
+//         return q.top();
+//     }
+// };
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int>q;
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            q.push(nums[i]);
+        priority_queue<int,vector<int>,greater<int>>p;
+        for(int i=0;i<k;i++){
+            p.push(nums[i]);
         }
-        for(int i=0;i<k-1;i++){
-            q.pop();
+        for(int i=k;i<n;i++){
+            p.push(nums[i]);
+            if(p.size()>k)p.pop();
         }
-        return q.top();
+        return p.top();
     }
 };
