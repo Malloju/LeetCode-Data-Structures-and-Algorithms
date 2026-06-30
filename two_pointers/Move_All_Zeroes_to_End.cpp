635b11d4e8626f8225ee65063767ad7c37733b57
Malloju Vishwam <152184740+Malloju@users.noreply.github.com>
2026-06-30 11:41:35 +0530

    Create Move_All_Zeroes_to_End.cpp

---
+++ two_pointers/Move_All_Zeroes_to_End.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        int n=arr.size();
+        int i=0;
+        int pos=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                arr[pos]=arr[i];
+                pos++;
+            }
+        }
+        for(int i=pos;i<n;i++){
+            arr[i]=0;
+        }
+    }
+};
